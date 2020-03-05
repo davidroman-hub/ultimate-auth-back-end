039cd9e5c9e8aea3fca71d8debb4684db573646b
davidroman-hub <jobroman83@gmail.com>
2020-03-05 19:42:05 +0100

    adding middlewares

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const app = express()
+const morgan =require('morgan')// for look in the console the method that we have(get, put, etc...)
+const cors = require('cors')// if you want to connect at the app at willl be run at the port 3000
+const bodyParser = require('body-parser')// for send the data to  maybe post mand and to have a response
+const mongoose = require('mongoose')
+require('dotenv').config()// acces to the variables in .env
+
 
 //import routes
 const authRoutes = require('./routes/auth')
 
 
+//app middlewares
+app.use(morgan('dev')); 
+app.use(bodyParser.json());
+// app.use(cors());// allowa all origins to accede to react etc..
+if(process.env.NODE_ENV = 'development'){
+    app.use(cors({origins:`http://localhost:3000`})); 
+}
+
 
 
 //middlewares
-app.use('/api/', authRoutes)
+app.use('/api/', authRoutes);
 
-const port = process.env.port || 8000 
+const port = process.env.PORT || 8000 
 app.listen(port, () => {
-    console.log(`API running on port ${port} `)
+    console.log(`API running on port ${port}`)
 })
+
+
+
