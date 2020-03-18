8bf7da630d45ecdee3fd7d2546638eac43ecbb1c
davidroman-hub <jobroman83@gmail.com>
2020-03-18 18:51:44 +0100

    update user method

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,14 @@ const router = express.Router()
 // import controller
 
 
-const{ read } = require('../controllers/user')
+const{ read, update } = require('../controllers/user')
 const { requireSignin} = require('../controllers/auth')
 
 
 //routes
 
 router.get('/user/:id', requireSignin , read)
+router.put('/user/update', requireSignin , update)
 
 
 
